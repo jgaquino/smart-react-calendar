e5840b734b3134d894828ea6d416b8cc5433abbe
Joao Gabriel <jgsantos@cash-converters.es>
2021-02-11 20:03:26 +0100

    dev env improved

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-import Calendar from './smart-react-calendar/dist'
+import CalendarDev from './smart-react-calendar/src'
+import CalendarProd from './smart-react-calendar/dist'
+
 import moment from 'moment-timezone'
 
 function App() {
+  const Calendar = process.env.REACT_APP_ENV === 'dev' ? CalendarDev : CalendarProd
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      
-      
-      <Calendar
-        selected={moment()}
-        startDate={moment()}
-        endDate={moment().add(1, 'months')}
-        disabledDays={['2021-02-01']}
-        onChange={newDate => console.log("HEEEEY ", newDate)}
-        format="dd MMMM D"
-      />
+    <>
+      <h1 style={{ textAlign: 'center', marginTop: '25px' }}>Calendar: <span style={{ color: "#2e88f1", textTransform: 'uppercase' }}>{process.env.REACT_APP_ENV}</span></h1>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
 
-      
-    </div>
+
+        <Calendar
+          selected={moment()}
+          startDate={moment()}
+          endDate={moment().add(1, 'months')}
+          disabledDays={['2021-02-01']}
+          onChange={newDate => console.log("HEEEEY ", newDate)}
+          format="dd MMMM D"
+        />
+
+
+      </div>
+    </>
   )
 }
 
